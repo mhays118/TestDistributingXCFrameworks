66c2d29c9ac9c890855ad8c6f0548d54efec03b8
Marcus Hays <dev@mhays.me>
2021-05-28 14:44:57 +0200

    added dummy files

---
+++ Sources/MainTargetWrapper/Include/dummy.h
@@ -0,0 +1,9 @@
+//
+//  dummy.h
+//  
+//
+//  Created by Marcus Hays on 5/28/21.
+//
+
+// Swift Package Manager needs at least one header to prevent a warning. See
+// https://github.com/firebase/firebase-ios-sdk/pull/6504.
---
+++ Sources/MainTargetWrapper/dummy.m
@@ -0,0 +1,8 @@
+//
+//  dummy.m
+//  
+//
+//  Created by Marcus Hays on 5/28/21.
+//
+
+// Swift Package Manager needs at least one source file.
