d490362703affbbf6d7797bebc867a202070ca59
Marcus Hays <dev@mhays.me>
2021-05-28 15:10:57 +0200

    Test removing wrapper project

--- Package.swift
+++ Package.swift
@@ -6,26 +6,19 @@ let package = Package(
     products: [
         .library(
             name: "MyWrappedFramework",
-            targets: ["MainTargetWrapper"]
+            targets: ["MainTarget", "CoreTarget"]
         )
     ],
     dependencies: [],
     targets: [
-        .target(
-            name: "MainTargetWrapper",
-            dependencies: [
-                "MainTarget",
-                "CoreTarget"
-            ]
-        ),
         .binaryTarget(
             name: "MainTarget",
-            url: "https://github.com/mhays118/TestDistributingXCFrameworks/releases/download/1.0.0/MainTarget.zip",
+            url: "https://github.com/mhays118/TestDistributingXCFrameworks/releases/download/1.0.1/MainTarget.zip",
             checksum: "8f0fb58303b9d0b69f4dcfdbcd589a91e69b0222047295b9ec7bbb6bc98b8f82"
         ),
         .binaryTarget(
             name: "CoreTarget",
-            url: "https://github.com/mhays118/TestDistributingXCFrameworks/releases/download/1.0.0/CoreTarget.zip",
+            url: "https://github.com/mhays118/TestDistributingXCFrameworks/releases/download/1.0.1/CoreTarget.zip",
             checksum: "6544fd6bde69feaeab92c5481930c5d8553fc73141546584f29b3756ae1d8a0d"
         ),
     ]
--- Sources/MainTargetWrapper/dummy.m
+++
@@ -1,8 +0,0 @@
-//
-//  dummy.m
-//  
-//
-//  Created by Marcus Hays on 5/28/21.
-//
-
-// Swift Package Manager needs at least one source file.
